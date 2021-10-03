fcb90d918219bc1cd066adcf7ed1711f05f2e6ed
Andrea Di Saverio <disaverio.andrea@gmail.com>
2021-10-03 17:58:13 +0200

    add csv printer

---
+++ src/main/kotlin/dev/disaverio/wlkreader/service/output/csv/CsvPrinter.kt
@@ -0,0 +1,26 @@
+package dev.disaverio.wlkreader.service.output.csv
+
+import dev.disaverio.wlkreader.models.data.DailySummary
+import dev.disaverio.wlkreader.models.data.WeatherDataRecord
+import dev.disaverio.wlkreader.service.output.FieldsListPrinter
+import dev.disaverio.wlkreader.service.output.Printer
+import org.apache.commons.csv.CSVFormat
+import org.apache.commons.csv.CSVPrinter
+import java.io.FileWriter
+
+class CsvPrinter(outputFieldsListFilename: String?): FieldsListPrinter(outputFieldsListFilename), Printer {
+
+    override fun printDailySummaries(elements: List<DailySummary>, outputPathname: String) =
+        print(elements, outputPathname)
+
+    override fun printDailyData(elements: List<WeatherDataRecord>, outputPathname: String) =
+        print(elements, outputPathname)
+
+    private inline fun<reified T> print(elements: List<T>, outputPathname: String) {
+        val printer = CSVPrinter(FileWriter(outputPathname), CSVFormat.DEFAULT);
+        printer.printRecord(getHeader<T>())
+        printer.printRecords(getRequestedFields(elements))
+        printer.flush()
+        printer.close()
+    }
+}
